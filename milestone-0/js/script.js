e24e6f9bc45b819ecf7d755fe52c23ba66ad6aa7
MirkoFarina <MirkoFarina@outlook.com>
2022-10-25 14:13:46 +0200

    [add] milestone 0

---
+++ milestone-0/js/script.js
@@ -0,0 +1,43 @@
+/**MILESTONE 0:**
+Creare l’array di oggetti con le informazioni fornite.
+*/
+const ourTeam = [
+  {
+    name: "Wayne",
+    lastName: "Barnett",
+    role: "Founder & CEO",
+    img: "wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    name: "Angela",
+    lastName: "Caroll",
+    role: "Chief Editor",
+    img: "angela-caroll-chief-editor.jpg"
+  },
+  {
+    name: "Walter",
+    lastName: "Gordon",
+    role: "Office Manager",
+    img: "walter-gordon-office-manager.jpg"
+  },
+  {
+    name: "Angela",
+    lastName: "Lopez",
+    role: "Social Media Manager",
+    img: "angela-lopez-social-media-manager.jpg"
+  },
+  {
+    name: "Scott",
+    lastName: "Estrada",
+    role: "Developer",
+    img: "scott-estrada-developer.jpg"
+  },
+  {
+    name: "Barbara",
+    lastName: "Ramos",
+    role: "Graphic Designer",
+    img: "barbara-ramos-graphic-designer.jpg"
+  }
+];
+
+console.table(ourTeam);
